bfd153b4b10a395efb6c9604aa58de16a237779c
Eric Tang <90581438+lag3n@users.noreply.github.com>
2022-01-15 13:44:37 -0500

    commit

---
+++ week1-exercises/personal-assistant/build/precache-manifest.6ab8592ccfe0f283eee1de45dd6ec6a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72e76b7a77033a81d30311544386741",
+    "url": "/personal-assistant/index.html"
+  },
+  {
+    "revision": "b6b0dc7fcd7a7ffe3526",
+    "url": "/personal-assistant/static/js/2.2acf81d9.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/personal-assistant/static/js/2.2acf81d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ff454792b4d88caf4c",
+    "url": "/personal-assistant/static/js/main.89e76c52.chunk.js"
+  },
+  {
+    "revision": "b8b8e24a3d2cb0a90037",
+    "url": "/personal-assistant/static/js/runtime-main.c67efb09.js"
+  }
+]);
